3387a9636fc712c28131e131d33b99e8e0503aed
philipfleischer <fleischer1@live.no>
2026-01-07 16:01:07 +0100

    Completed the Advanced functionality for the chatroom with ban and kick for tha admin user

--- Chat_room/server.py
+++ Chat_room/server.py
@@ -1,4 +1,3 @@
-from ast import arg
 import threading
 import socket
 
@@ -21,16 +20,36 @@ def broadcast(message):
 def handle(client):
     while True:
         try:
-            message = client.recv(1024)  # bytes
-            broadcast(message)
+            # Just making it point to the same place, due to looping problems
+            msg = message = client.recv(1024)  # bytes 1024
+            if not msg:
+                raise ConnectionError("Client disconnected")
+            if msg.decode("ascii").startswith("KICK"):
+                if nicknames[clients.index(client)] == "admin":
+                    name_to_kick = msg.decode("ascii")[5:]
+                    kick_user(name_to_kick)
+                else:
+                    client.send("Command was refused!".encode("ascii"))
+            elif msg.decode("ascii").startswith("BAN"):
+                if nicknames[clients.index(client)] == "admin":
+                    name_to_ban = msg.decode("ascii")[4:]
+                    kick_user(name_to_ban)
+                    with open("bans.txt", "a") as f:
+                        f.write(f"{name_to_ban}\n")
+                    print(f"{name_to_ban} was Banned!")
+                else:
+                    client.send("Command was refused!".encode("ascii"))
+            else:
+                broadcast(message)
         except:
-            index = clients.index(client)  # Index of failed client
-            clients.remove(client)
-            client.close()
-            nickname = nicknames[index]
-            broadcast(f"{nickname} left the chat!".encode("ascii"))
-            nicknames.remove(nickname)
-            break
+            if client in clients:
+                index = clients.index(client)
+                clients.remove(client)
+                client.close()
+                nickname = nicknames[index]
+                broadcast(f"{nickname} left the chat!".encode("ascii"))
+                nicknames.remove(nickname)
+                break
 
 
 def receive():
@@ -40,6 +59,24 @@ def receive():
 
         client.send("NICK".encode("ascii"))
         nickname = client.recv(1024).decode("ascii")
+
+        with open("bans.txt", "r") as f:
+            bans = f.readlines()
+
+        if nickname + "\n" in bans:
+            client.send("BAN".encode("ascii"))
+            client.close()
+            continue
+
+        if nickname == "admin":
+            client.send("PASSWD".encode("ascii"))
+            password = client.recv(1024).decode("ascii")
+
+            if password != "adminpasswd":
+                client.send("REFUSE".encode("ascii"))
+                client.close()
+                continue  # Not break, so we can continue on wron passwd
+
         nicknames.append(nickname)
         clients.append(client)
 
@@ -49,6 +86,17 @@ def receive():
 
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
+
+
+def kick_user(name):
+    if name in nicknames:
+        name_index = nicknames.index(name)
+        client_to_kick = clients[name_index]
+        clients.remove(client_to_kick)
+        client_to_kick.send("You were kicked by an admin!".encode("ascii"))
+        client_to_kick.close()
+        nicknames.remove(name)
+        broadcast(f"{name} was kicked by an admin!".encode("ascii"))
 
 
 print("Server is listening...")
