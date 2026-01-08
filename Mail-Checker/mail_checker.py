96eaf10064cbfeec752a656230dfedb7cf98af44
philipfleischer <fleischer1@live.no>
2026-01-08 09:47:35 +0100

    Mail checker using imap completed, We can check mail in Inbox and sent email in a human readable way

--- Mail-Checker/mail_checker.py
+++ Mail-Checker/mail_checker.py
@@ -16,3 +16,23 @@ imap.login(email_address, password)
 imap.select("Inbox")
 
 # Iterate over messages in "Inbox"
+_, msgnums = imap.search(None, "ALL")
+# print(msgnums) -> [b'1 2 3 4']
+for msgnum in msgnums[0].split():
+    # Fetching the data in the emails:
+    _, data = imap.fetch(msgnum, "(RFC822)")
+    # print(data)
+    message = email.message_from_bytes(data[0][1])  # Message object
+
+    print(f"Message Number: {msgnum}")
+    print(f"From: {message.get("From")}")
+    print(f"To: {message.get("To")}")
+    print(f"BCC: {message.get("BCC")}")
+    print(f"Date: {message.get("Date")}")
+    print(f"Subject: {message.get("Subject")}")
+    print("Content:")
+    for part in message.walk():  # Parts yielded by the walk() func
+        if part.get_content_type() == "text/plain":
+            print(part.as_string())
+
+imap.close()
